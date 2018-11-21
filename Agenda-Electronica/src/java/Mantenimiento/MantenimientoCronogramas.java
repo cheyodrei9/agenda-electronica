281aceadbec0d7e1b5f1df684d4ccbd55d589693
Eliseo Emmanuel Garcia Alvarez <eliseo.garciausam@sv.cds>
2018-11-21 08:28:46 -0600

    modificacion de mantenimiento cronograma
    cronogramas

--- Agenda-Electronica/src/java/Mantenimiento/MantenimientoCronogramas.java
+++ Agenda-Electronica/src/java/Mantenimiento/MantenimientoCronogramas.java
@@ -6,7 +6,9 @@
 package Mantenimiento;
 
 import Persistencia.Cronogramas;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  *
@@ -33,4 +35,85 @@ public class MantenimientoCronogramas {
         return flag;
     }
     
+    public Cronogramas consultarid(int idCronograma) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        Cronogramas cronogramas = null;
+        em.getTransaction().begin();
+        try {
+            cronogramas = em.find(Cronogramas.class, idCronograma);
+            em.getTransaction().commit();
+            System.out.println("exito consultarid");
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            System.out.println("error consultarid");
+        } finally {
+            em.close();
+        }
+        return cronogramas;
+    }
+    
+     public List<Cronogramas> consultar() {
+        List<Cronogramas> listaCR = null;
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        try{
+            Query query=em.createQuery("SELECT c FROM Cronogramas c");
+            em.getTransaction().commit();
+            listaCR=query.getResultList();
+            System.out.println(listaCR+"si");
+            return listaCR;
+        }catch(Exception e){
+            em.getTransaction().rollback();
+            return null;
+        }finally{
+            em.close();
+        }
+    }
+     
+        public int Actualizar(Cronogramas cronogramas){
+        EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
+        Cronogramas crono=null;
+        em.getTransaction().begin();
+        int flag=0;
+        try{
+            crono=em.find(Cronogramas.class,cronogramas.getIdCronograma());
+            crono.setIdCronograma(cronogramas.getIdCronograma());
+            crono.setNombre(cronogramas.getNombre());
+            crono.setDescripcion(cronogramas.getDescripcion());
+            crono.setIdUsuario(cronogramas.getIdUsuario());
+            
+            em.getTransaction().commit();
+            flag=1;
+            System.out.println("exitoso");
+        }catch(Exception e){
+            em.getTransaction().rollback();
+            flag=0;
+            System.out.println("error"+e);
+        }finally{
+            em.close();
+        }
+        return flag;
+    }
+        
+         public int eliminar(Cronogramas cronogramas){
+      EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
+      Cronogramas cro=null;
+      em.getTransaction().begin();
+      int flag=0;
+      try{
+          cro=em.find(Cronogramas.class,cronogramas.getIdCronograma());
+          em.remove(cro);
+          em.getTransaction().commit();
+          flag=1;
+          System.out.println("exito al eliminar");
+      }catch(Exception e){
+          em.getTransaction().rollback();
+          flag=0;
+      }finally{
+          em.close();
+      }
+      return flag;
+    }
 }
+    
+
