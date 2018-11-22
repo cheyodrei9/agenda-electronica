425dbbdffe617d7bc1cdd396fa6f098a1914bdde
Eliseo Emmanuel Garcia Alvarez <eliseo.garciausam@sv.cds>
2018-11-22 15:26:37 -0600

    Update NewClass.java
    prueba

--- Agenda-Electronica/test/prueba/NewClass.java
+++ Agenda-Electronica/test/prueba/NewClass.java
@@ -5,12 +5,35 @@
  */
 package prueba;
 
+import Mantenimiento.MantenimientoActividades;
+import Persistencia.Actividades;
+import Persistencia.Cronogramas;
+import Persistencia.Fases;
+import Persistencia.TiposActividades;
+
 /**
  *
  * @author eliseo.garciausam
  */
 public class NewClass {
     public static void main(String[] args) {
+        Actividades actividades = new Actividades();
+        Cronogramas cronogramas = new Cronogramas();
+        TiposActividades tipos = new TiposActividades();
+        Fases fases = new Fases();
+        MantenimientoActividades act = new MantenimientoActividades();
+        
+        actividades.setIdActividad(0);
+        cronogramas.setIdCronograma(1);
+        actividades.setIdCronograma(cronogramas);
+        tipos.setIdTipoActividad(1);
+        actividades.setIdTipoActividad(tipos);
+        actividades.setNombreaActividad("hola");
+        actividades.setFechaActividad();
+        fases.setIdFase(1);
+        actividades.setIdFases(fases);
+        
+        
         
     }
 }
